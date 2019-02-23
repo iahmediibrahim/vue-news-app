8d4f93afe80814d00380f720d96d8ebfe436cef9
AITCWEB <aitc.dev@gmail.com>
2019-02-23 15:07:59 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+ <template>
+  <v-app light>
+    <SideMenu :drawer="drawer" :api_key="api_key" @selectsource="setResource"></SideMenu>
+    <!--The Side Menu component go here -->
+    <v-toolbar fixed app light clipped-left color="primary" class="elevation-2">
+      <v-toolbar-side-icon @click="drawer = !drawer" class="white--text"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">News App</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <!--The Main Content component go here-->
+        <MainContent :articles="articles"></MainContent>
+      </v-container>
+    </v-content>
+    <v-footer class="secondary" app>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <div class="white--text ml-3">
+            Made by
+            <a
+              class="white--text"
+              href="https://github.com/rachidsakara"
+              target="_blank"
+            >Ahmed Ibrahim</a>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+import axios from "axios"; // importing the axios (a HTTP library) to connects the app with the News API
+import MainContent from "./components/MainContent.vue"; // import the Main Content component
+import SideMenu from "./components/SideMenu.vue"; // import the SideMenu component
+
+export default {
+  components: {
+    MainContent,
+    SideMenu // Register the component
+  },
+  data() {
+    return {
+      drawer: false, // false = Vuetify automatically "do the right thing" to show/hide the drawer
+      api_key: "b9929503582240a49dcab3efc9d3eb67", // Your API Key go here
+      articles: [],
+      errors: []
+    };
+  },
+  created() {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=" +
+          this.api_key
+      )
+      .then(response => {
+        //this.articles = response.data.articles
+        this.articles = response.data.articles;
+        console.log("data:");
+        console.log(response.data.articles); // This will give you access to the full object
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  },
+  methods: {
+    setResource(source) {
+      axios
+        .get(
+          "https://newsapi.org/v2/top-headlines?sources=" +
+            source +
+            "&apiKey=" +
+            this.api_key
+        )
+        .then(response => {
+          this.articles = response.data.articles;
+          console.log(response.data);
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    }
+  }
+};
+</script>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap align-center>
+      <v-flex v-for="article in articles" :key="article.title" xs12 md6>
+        <div order-md2>
+          <v-card class="ma-3" hover data-aos="zoom-out-down" data-aos-easing="ease">
+            <v-responsive>
+              <v-img height="350px" v-bind:src="article.urlToImage"></v-img>
+              <v-container fill-height fluid>
+                <v-layout>
+                  <v-flex xs12 align-start d-flex>
+                    <span class="headline">{{ article.title }}</span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-responsive>
+            <v-card-text>{{ article.description }}</v-card-text>
+            <v-card-actions>
+              <v-avatar size="32px">
+                <img class="img-circle elevation-7 mb-1" :src="getImgUrl(article.source.id)">
+              </v-avatar>
+              <v-chip small class="dark--text">{{article.source.name}}</v-chip>
+              <v-spacer></v-spacer>
+              <!-- <v-chip small color="blue lighten-1" class="dark--text">{{article.author}}</v-chip>
+              <v-spacer></v-spacer>-->
+              <v-btn
+                small
+                replace
+                color="primary"
+                v-bind:href="article.url"
+                target="_blank"
+              >Read More</v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    articles: Array
+  },
+
+  methods: {
+    getImgUrl(pic) {
+      return require("../assets/images/" + pic + ".png");
+    }
+  }
+};
+</script>
