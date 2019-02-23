2266fbe4060d9dab3e67b757ab501db9cf7f5b27
AITCWEB <aitc.dev@gmail.com>
2019-02-23 15:46:59 +0200

    deploy

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-news-app/dist/',
+    assetsPublicPath: 'https://aitcweb.github.io/vue-news-app',
     proxyTable: {},
 
     // Various Dev Server settings
